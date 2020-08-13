529bf455d40356c5e271fee09b195bdd796cbee9
liuyunjs <429695034@qq.com>
2020-08-13 21:39:51 +0800

    0.0.2 添加第二个参数为依赖, 依赖更新时重建引用

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
-export function useCallback<T extends (...args: any[]) => any>(callback: T) {
+export function useCallback<T extends (...args: any[]) => any>(callback: T, deps: any[] = []) {
   const ref = React.useRef<T>();
 
   ref.current = callback;
 
   return React.useCallback(function (...args: any[]) {
     return ref.current!(...args);
-  }, []);
+  }, deps);
 }
 
 export default useCallback;
